88dd71e8d848fc7b831c5690861742f52f2f7cd3
Карпенко Глеб <glebkarpenko1@gmail.com>
2024-04-01 13:32:51 +0300

    add word dialog

--- wordum/lib/views/dictionary_page.dart
+++ wordum/lib/views/dictionary_page.dart
@@ -2,19 +2,59 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:wordum/view_models/word_view.dart';
 
-class DictionaryPage extends StatelessWidget {
+class DictionaryPage extends StatefulWidget {
   const DictionaryPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<WordView>();
+  State<DictionaryPage> createState() => _DictionaryPageState();
+}
 
+class _DictionaryPageState extends State<DictionaryPage>{  
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        const Center(
+          child: WordList(),
+        ),
+        Positioned(
+          bottom: 20,
+          right: 20,
+          child: SizedBox(
+            width: 66,
+            height: 66,
+            child: FittedBox(
+              child: FloatingActionButton(
+                backgroundColor: Theme.of(context).colorScheme.onPrimary,
+                onPressed: (){
+                  showDialog(
+                    context: context, 
+                    builder: (BuildContext context){
+                      return AddWordDialog();
+                    },
+                  );
+                },
+                child: const Icon(Icons.add),
+              ),
+            ),
+          )
+        ),
+      ],
+    );
+  }
+}
+
+class WordList extends StatelessWidget {
+  const WordList({super.key});
+
+  @override 
+  Widget build(BuildContext context){
+    var appState = context.watch<WordView>();
     if (appState.wordsToLearn.isEmpty) {
       return const Center(
         child: Text('No words to learn yet'),
       );
     }
-
     return ListView(
       children: [
         Padding(
@@ -27,6 +67,48 @@ class DictionaryPage extends StatelessWidget {
             leading: const Icon(Icons.edit),
             title: Text(word),
           ),
+      ],
+    );
+  }
+}
+
+class AddWordDialog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Add a word'),
+      content: const Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Enter the word'
+            ),
+          ),
+          SizedBox(
+            height: 10,
+            ),
+          TextField(
+            readOnly: true,
+            decoration: InputDecoration(
+              labelText: 'Translation'
+            ),
+          ),
+        ],
+      ),
+      actions: [
+        TextButton(
+          onPressed: (){
+            Navigator.of(context).pop();
+          },
+          child: const Text('Add'),
+        ),
+        TextButton(
+          onPressed: (){
+            Navigator.of(context).pop();
+          },
+          child: const Text('Cancel'),
+        )
       ],
     );
   }
