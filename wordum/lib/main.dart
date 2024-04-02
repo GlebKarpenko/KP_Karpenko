40b033c230b059a57a73b8012f9d68ff378b4db9
Gleb Karpenko <glebkarpenko1@gmail.com>
2024-04-02 22:52:02 +0300

    add user settings model

--- wordum/lib/main.dart
+++ wordum/lib/main.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:wordum/models/dictionary.dart';
+import 'package:wordum/models/settings.dart';
 import 'package:wordum/models/word_generator.dart';
 import 'package:wordum/views/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await UserSettings.init();
   await Dictionary.init();
 
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  static List<int> argb = [255, 181, 113, 41];
+
+  MyApp({super.key}) {
+    setARGB();
+  }
+
+  static void setARGB(){
+    List<int> newARGB = UserSettings.getColorShemeARGB();
+    if (newARGB.length == 4){
+      argb = newARGB;
+    }
+    else {
+      debugPrint('Could not get personlized color sheme, using default');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +40,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(
-              seedColor: const Color.fromARGB(255, 181, 113, 41)),
+              seedColor: Color.fromARGB(argb[0], argb[1], argb[2], argb[3])),
         ),
         home: const MyHomePage(),
       ),
---
+++ wordum/lib/models/settings.dart
@@ -0,0 +1,40 @@
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+class UserSettings {
+  static late SharedPreferences _preferences;
+  static const _favLanguageKey = 'favouriteLanguage';
+  static const _favLanguageCodeKey = 'favouriteLanguageCode';
+  static const _colorShemeARGBKey = 'colorShemeARGB';
+  static const String NONE = '';
+
+  static Future init() async {
+    _preferences = await SharedPreferences.getInstance();
+  }
+
+  static Future<void> setColorShemeARGB (List<int> argb) async {
+    String convertedARGB = argb.map((value) => value.toString()).join(';');
+    _preferences.setString(_colorShemeARGBKey, convertedARGB);
+  }
+
+  static Future<void> setFavLanguage(String favLanguage) async {
+    _preferences.setString(_favLanguageKey, favLanguage);
+  }
+
+  static Future<void> setFavLanguageCode(String favLanguageCode) async {
+    _preferences.setString(_favLanguageCodeKey, favLanguageCode);
+  }
+
+  static List<int> getColorShemeARGB(){
+    String convertedARGB = _preferences.getString(_colorShemeARGBKey) ?? NONE;
+    return convertedARGB == NONE ? []: convertedARGB.split(';').map((value) => int.parse(value)).toList(); 
+  }
+
+  static String getFavLanguage(){
+    return _preferences.getString(_favLanguageKey) ?? NONE;
+  }
+
+  static String getFavLanguageCode(){
+    return _preferences.getString(_favLanguageCodeKey) ?? NONE;
+  }
+}
