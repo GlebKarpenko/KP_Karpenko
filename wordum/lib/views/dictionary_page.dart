a306dc062f49376c58b803f06d544b1f8f438240
Gleb Karpenko <glebkarpenko1@gmail.com>
2024-04-08 14:21:39 +0300

    remove add word dialog from widgets

--- wordum/lib/views/dictionary_page.dart
+++ wordum/lib/views/dictionary_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:wordum/models/dictionary.dart';
 import 'widgets/positioned_button.dart';
 import 'widgets/draggable_list_item.dart';
-import 'widgets/add_word_dialog.dart';
 
 class DictionaryPage extends StatefulWidget {
   const DictionaryPage({super.key});
@@ -43,7 +42,7 @@ class _DictionaryPageState extends State<DictionaryPage> {
               showDialog(
                 context: context, 
                 builder: (BuildContext context){
-                  return AddWordDialog(
+                  return _AddWordDialog(
                     onAdd: fetchWordList
                   );
                 },
@@ -112,6 +111,67 @@ class TrashCan extends StatelessWidget {
           );
         },
       ),
+    );
+  }
+}
+
+class _AddWordDialog extends StatefulWidget {
+  final VoidCallback onAdd;
+  const _AddWordDialog({required this.onAdd});
+
+  @override
+  State<_AddWordDialog> createState() => _AddWordDialogState();
+}
+
+class _AddWordDialogState extends State<_AddWordDialog> {
+  String _inputWord = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Add a word'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextField(
+            decoration: const InputDecoration(
+              labelText: 'Enter the word'
+            ),
+            onChanged: (String newValue) {
+              setState(() {
+                _inputWord = newValue;
+              });
+            },
+          ),
+          const SizedBox(
+            height: 10,
+            ),
+          const TextField(
+            readOnly: true,
+            decoration: InputDecoration(
+              labelText: 'Translation',
+            ),
+          ),
+        ],
+      ),
+      actions: [
+        TextButton(
+          onPressed: (){
+            if (_inputWord.isNotEmpty){
+              Dictionary.addWord(_inputWord);
+              Navigator.of(context).pop();
+              widget.onAdd();
+            }
+          },
+          child: const Text('Add'),
+        ),
+        TextButton(
+          onPressed: (){
+            Navigator.of(context).pop();
+          },
+          child: const Text('Cancel'),
+        )
+      ],
     );
   }
 }
